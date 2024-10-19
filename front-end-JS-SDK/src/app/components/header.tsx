88058a58e0ba30f9ff311ec6f90e1b8895f021be
alex njoya <njoyaalexander71@gmail.com>
2024-10-19 07:13:27 +0000

    validator

--- front-end-JS-SDK/src/app/components/header.tsx
+++ front-end-JS-SDK/src/app/components/header.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState } from 'react'; // Added useState for managing wallet connection
 import { ethers } from 'ethers'; 
-
+import Link from 'next/link'; // Import Link from next/link
 
 const Header: React.FC = () => {
   const [walletAddress, setWalletAddress] = useState<string | null>(null); // State to store wallet address
@@ -9,7 +9,6 @@ const Header: React.FC = () => {
   const connectWallet = async () => {
     if (window.ethereum) {
       try {
-       
         const provider = new ethers.BrowserProvider(window.ethereum); 
         const accounts = await provider.send("eth_requestAccounts", []); 
         if (accounts && accounts.length > 0) {
@@ -28,26 +27,31 @@ const Header: React.FC = () => {
 
   return (
     <header className="p-4 mt-10 ">
-      <nav className="flex fixed top-0 mt-5  justify-end items-center">
-        <div className='flex  justify-start'>
+      <nav className="flex fixed top-0 mt-5 justify-end items-center">
+        <div className='flex justify-start'>
           <h1 className="text-white hover:text-gray-300 ml-2 flex justify-start text-2xl">MoveMe</h1>
         </div>
         <div className='flex justify-end ml-20'>
           <ul className="flex justify-end space-x-6 ml-auto">
-            <li><a className="text-white flex justify-end hover:text-gray-300" href="#">All-time-top</a></li>
-            <li><a className="text-white flex justify-end hover:text-gray-300" href="#Transactions">Transactions</a></li>
-            <li><a className="text-white bg-red-500 p-1 rounded-md flex justify-end hover:text-gray-300" href="#upcoming">Upcoming</a></li>
-            
+            <li>
+              <Link href="/" className="text-white flex justify-end hover:text-gray-300">All-time-top</Link>
+            </li>
+            <li>
+              <Link href="/transactions" className="text-white flex justify-end hover:text-gray-300">Transactions</Link>
+            </li>
+            <li>
+              <Link href="/upcoming" className="text-white bg-red-500 p-1 rounded-md flex justify-end hover:text-gray-300">Upcoming</Link>
+            </li>
           </ul>
         </div>
-        <div className="ml-[600px]">
-            <button 
-              onClick={connectWallet} 
-              className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            >
-              {walletAddress ? `Connected` : 'Connect Wallet'}
-            </button> 
-          </div>
+        <div className="ml-[550px]">
+          <button 
+            onClick={connectWallet} 
+            className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            {walletAddress ? `Connected` : 'Connect Wallet'}
+          </button> 
+        </div>
       </nav>
     </header>
   );
